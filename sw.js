2194ffbfa29746beeef533ef6f5429c82f44a51e
CaesarImperator-hash <nilessaremi@gmail.com>
2026-09-02 15:09:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pl-tracker-v11';
+const CACHE_NAME = 'pl-tracker-v12';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png', '/favicon-32.png'];
 
 self.addEventListener('install', (e) => {
